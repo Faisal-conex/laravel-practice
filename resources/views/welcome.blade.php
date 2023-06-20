5266225e089860867edf4f2e053cb4a10f44e6e6
Faisal-conex <md.faisal@diaminter.com>
2023-06-20 16:05:57 -0400

    first batch commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>title</title>
+</head>
+
+<body>
+    <div id="app">
+        <h1>Welcome </h1>
+        <a href="about">About</a>
+        <a href="contact">Contact</a>
+    </div>
+
+
+</body>
+
+</html>
---
+++ routes/web.php
@@ -0,0 +1,84 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Users;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+//passing parameters and now the route / can accept the name parameter as well
+/* Route::get('/{name}', function ($name) {
+
+    //we can pass parameters to the VIEW through array
+    //the key is the actual variable that is passed to the BLADE FILE
+    return view('welcome', ['key' => $name]);
+}); */
+
+//longhand
+/* Route::get('/about', function(){
+    return view('about');
+});
+ */
+
+//shorthand  /path, view
+Route::view("about", "about");
+Route::view("contact", "contact");
+
+
+
+
+
+//controllers
+//contoller syntax Route:get("path", "controlelr file");
+
+Route::get("users", [Users::class, 'index']);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
